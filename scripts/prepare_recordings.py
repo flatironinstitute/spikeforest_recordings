ee49393d8a481bfdc1cc8f89f6bf2d955274e7bd
Jeremy Magland <jmagland@flatironinstitute.org>
2019-11-15 09:23:24 -0500

    self_reference!

--- scripts/prepare_recordings.py
+++ scripts/prepare_recordings.py
@@ -60,12 +60,14 @@ def main():
                             params=ka.load_object(recdir + '/params.json'),
                             geom=np.genfromtxt(ka.load_file(recdir + '/geom.csv'), delimiter=',').T
                         )
+                        obj['self_reference'] = ka.store_object(obj, basename='{}/{}/{}.json'.format(studyset_name, study_name, recname)
                         with open(recfile, 'w') as f:
                             json.dump(_json_serialize(obj), f, indent=4)
                         firings_true_file = os.path.join(studydir_local, recname + '.firings_true.json')
                         obj2 = dict(
                             firings=recfile + '/firings_true.mda'
                         )
+                        obj2['self_reference'] = ka.store_object(obj2, basename='{}/{}/{}.firings_true.json'.format(studyset_name, study_name, recname)
                         with open(firings_true_file, 'w') as f:
                             json.dump(obj2, f, indent=4)
 
