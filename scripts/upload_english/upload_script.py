5f0b4c4ec33e06d25f054fedd26a6fbfbdfadb43
Jeremy Magland <jmagland@flatironinstitute.org>
2020-03-11 15:24:33 -0400

    update upload_script for dan english data

--- scripts/upload_english/upload_script.py
+++ scripts/upload_english/upload_script.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
 
+from os.path import basename
 import numpy as np
 import json
-from spikeforest2_utils import AutoRecordingExtractor, MdaRecordingExtractor
+from spikeforest2_utils import AutoRecordingExtractor, MdaRecordingExtractor, MdaSortingExtractor
 import hither
 import kachery as ka
 import os
 
 studyset_name = 'PAIRED_ENGLISH'
-study_name = 'PAIRED_ENGLISH'
+study_name = 'paired_english'
 path_from = '/mnt/ceph/users/jjun/groundtruth/paired_english'
 path_to = '/mnt/home/jjun/src/spikeforest_recordings/recordings/PAIRED_ENGLISH/paired_english'
 
@@ -54,9 +55,16 @@ ka.set_config(
 list_rec = [str(f) for f in os.listdir(path_from) if os.path.isdir(os.path.join(path_from, f))]
 
 print('# files: {}'.format(len(list_rec)))
+study_obj = dict(
+    name=study_name,
+    studySetName=studyset_name,
+    recordings=[]
+)
 for rec1 in list_rec:
     print(f'Uploading {rec1}')
     path_rec1 = os.path.join(path_from, rec1)
+    rec = MdaRecordingExtractor(path_rec1)
+    sorting = MdaSortingExtractor(path_rec1 + '/firings_true.mda')
     if False:
         register_recording(
             recdir = path_rec1, 
@@ -71,12 +79,41 @@ for rec1 in list_rec:
             label = rec1+'.firings_true',
             to = 'default_readwrite'
         )
+    recording_obj = dict(
+        name=rec1,
+        studyName=study_name,
+        studySetName=studyset_name,
+        directory=ka.store_dir(path_rec1),
+        firingsTrue=ka.store_file(os.path.join(path_to, rec1+'.firings_true.json'), basename='firings_true.json'),
+        sampleRateHz=rec.get_sampling_frequency(),
+        numChannels=len(rec.get_channel_ids()),
+        durationSec=rec.get_num_frames() / rec.get_sampling_frequency(),
+        numTrueUnits=len(sorting.get_unit_ids()),
+        spikeSign=-1 # TODO: get this from params.json
+    )
+    study_obj['recordings'].append(recording_obj)
 
-# write to PAIRED_ENGLISH.json
-print(f'STUDYSET: {studyset_name}')
-print('STUDY: {}/{}'.format(studyset_name, study_name))
-studydir_local = path_to
+study_obj['self_reference'] = ka.store_object(study_obj)
+with open(os.path.join(path_to, studyset_name, study_name, study_name + '.json'), 'w') as f:
+    json.dump(study_obj, f, indent=4)
 
-study['self_reference'] = ka.store_object(study, basename='{}.json'.format(study_name))
-with open(os.path.join(path_to, studyset_name + '.json'), 'w') as f:
-    json.dump(study, f, indent=4)
+studyset_obj = dict(
+    name=studyset_name,
+    info=dict(
+        label=studyset_name,
+        electrode_type='[electrode_type]',
+        doi='[doi]',
+        ground_truth='[ground_truth]',
+        organism='[organism]',
+        source='[source]',
+        labels=["in-vivo"]
+    ),
+    description='[description]'
+    studies=[
+        study_obj
+    ]
+)
+
+studyset_obj['self_reference'] = ka.store_object(studyset_obj)
+with open(os.path.join(path_to, studyset_name, studyset_name + '.json'), 'w') as f:
+    json.dump(studyset_obj, f, indent=4)
