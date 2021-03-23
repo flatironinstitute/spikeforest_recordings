a97fee96d18f7b68a8eef42debbd23a3662791bc
Jeff Soules <jsoules@flatironinstitute.org>
2021-03-23 12:11:53 -0400

    Support hither, cleaned-up approach in fetch_metrics.py.

--- scripts/fetch_metrics.py
+++ scripts/fetch_metrics.py
@@ -1,22 +1,63 @@
 #!/usr/bin/python
 
+import argparse
 import json
 from spikeextractors import RecordingExtractor, SortingExtractor
 from spikecomparison import GroundTruthComparison
 import spiketoolkit as st
-from typing import Any, Dict, List, Set, Tuple
+from typing import Any, cast, Dict, List, Set, Tuple, TypedDict, Union
 
+import hither2 as hi
 import kachery_p2p as kp
 import labbox_ephys as le
 
-VERBOSE = True
-VERY_VERBOSE = True
-ABBREVIATED_TEST = True
-SORTINGS = '/home/jsoules/src/spikeforest_recordings/sortings.json'
+class ArgsDict(TypedDict):
+    verbose: int
+    test: int
+    sortingsfile: str
+    recordingset: str
+
+args: Union[ArgsDict, None] = None
 
 RECORDING_URI_KEY = 'recordingUri'
 GROUND_TRUTH_URI_KEY = 'sortingTrueUri'
+SORTING_FIRINGS_URI_KEY = 'firings'
+QUALITY_METRICS = [
+    "num_spikes",
+    "firing_rate",
+    "presence_ratio",
+    "isi_violation",
+    "amplitude_cutoff",
+    "snr",
+    "max_drift",
+    "cumulative_drift",
+    "silhouette_score",
+    "isolation_distance",
+    "l_ratio",
+    "nn_hit_rate",
+    "nn_miss_rate",
+    "d_prime"
+]
 
+def init_args():
+    global args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='count', default=0)
+    parser.add_argument('--test', '-t', action='count', default=0,
+        help="If non-zero, this will set a maximum number of iterations before quitting, " +
+        "to give a usable sample without processing the entire data set.")
+    parser.add_argument('--sortingsfile', '-s', action='store',
+        default='/home/jsoules/src/spikeforest_recordings/sortings.json',
+        help="The full path for the JSON file which contains the sortings. The sortings file content " +
+            "should be equivalent to the output of an API call to SpikeForest.")
+    parser.add_argument('--recordingset', '-r', action='store', default='',
+        help='If set, will limit processing to the set of recordings named in the variable (e.g. "paired_kampff").')
+    args = cast(ArgsDict, parser.parse_args())
+
+def print_per_verbose(lvl: int, msg: str):
+    if args['verbose'] < lvl: return
+    tabs = max(0, lvl - 1)
+    print("\t" * tabs + msg)
 
 def slurp(filename: str) -> str:
     as_one_string = ''
@@ -28,20 +69,13 @@ def slurp(filename: str) -> str:
     return as_one_string
 
 def load_sortings() -> List[Dict[str, Any]]:
-    hydrated_sortings = json.loads(slurp(SORTINGS))
+    hydrated_sortings = json.loads(slurp(args['sortingsfile']))
     return hydrated_sortings
 
-
 def compute_quality_metrics(recording: RecordingExtractor, sorting: SortingExtractor) -> str:
-    metric_names = [
-        "num_spikes", "firing_rate", "presence_ratio",
-        "isi_violation", "amplitude_cutoff", "snr", "max_drift",
-        "cumulative_drift", "silhouette_score", "isolation_distance",
-        "l_ratio", "nn_hit_rate", "nn_miss_rate","d_prime"]
-
     return st.validation.compute_quality_metrics(
         sorting, recording,
-        metric_names=metric_names, as_dataframe=True).to_dict()
+        metric_names=QUALITY_METRICS, as_dataframe=True).to_dict()
 
 def compare_with_ground_truth(sorting: SortingExtractor, gt_sorting: SortingExtractor):
     ground_truth_comparison = GroundTruthComparison(gt_sorting, sorting)
@@ -50,51 +84,51 @@ def compare_with_ground_truth(sorting: SortingExtractor, gt_sorting: SortingExtr
             "best_match_12": ground_truth_comparison.best_match_12.to_list(),
             "agreement_scores": ground_truth_comparison.agreement_scores.to_dict()}
 
-def compute_quality_metrics_json(recording: RecordingExtractor, sorting: SortingExtractor) -> str:
-    metric_names = [
-        "num_spikes", "firing_rate", "presence_ratio",
-        "isi_violation", "amplitude_cutoff", "snr", "max_drift",
-        "cumulative_drift", "silhouette_score", "isolation_distance",
-        "l_ratio", "nn_hit_rate", "nn_miss_rate","d_prime"]
+@hi.function(
+    'compute_quality_metrics_hi', '0.1.0',
+    kachery_support=True
+)
+def compute_quality_metrics_hi(recording_uri, gt_uri, firings_uri):
+    # gt_uri is not needed, but including it lets this method and the ground truth comparison use the same consistent kwargs parameters.
+    print_per_verbose(1, f"Computing quality metrics for recording {recording_uri} and sorting {firings_uri}. Fetching Extractors...")
+    print_per_verbose(2, f"Execting le.LabboxEphysRecordingExtractor({recording_uri})")
+    recording = le.LabboxEphysRecordingExtractor(recording_uri)
+    sample_rate = recording.get_sampling_frequency()
+    print_per_verbose(2, f"Found sample rate {sample_rate}.")
+    sorting_object = {
+        'sorting_format': 'mda',
+        'data': {
+            'firings': firings_uri,
+            'samplerate': sample_rate
+        }
+    }
+    print_per_verbose(2, f"(Comparison evaluation) Executing le.labboxEphysSortingExtractor({json.dumps(sorting_object)})")
+    sorting = le.LabboxEphysSortingExtractor(sorting_object)
+    print_per_verbose(2, f"Executing quality metrics")
+    return compute_quality_metrics(recording, sorting)
 
-    return st.validation.compute_quality_metrics(
-        sorting, recording,
-        metric_names=metric_names, as_dataframe=True).to_json()
-
-def compare_with_ground_truth_jsonstring(sorting: SortingExtractor, gt_sorting: SortingExtractor):
-    ground_truth_comparison = GroundTruthComparison(gt_sorting, sorting)
-
-    return json.dumps({"best_match_21": ground_truth_comparison.best_match_21.to_list(),
-            "best_match_12": ground_truth_comparison.best_match_12.to_list(),
-            "agreement_scores": ground_truth_comparison.agreement_scores.to_json()})
-
-def get_unique_recordings_from_sortresult_list(sorting_entries: List[Dict[str, Any]]) -> Dict[str, str]:
-    if VERY_VERBOSE: print(json.dumps(sorting_entries[0]))
-    recording_set = set([a[RECORDING_URI_KEY] for a in sorting_entries])
-
-    recording_uris_mapped_to_true_firings = {}
-    for recording in recording_set:
-        if VERBOSE: print(f"Checking recording {recording}")
-        # We expect every recording (identified here by the URI) to be uniquely associated with a ground truth set.
-        potential_gts = set([a[GROUND_TRUTH_URI_KEY] for a in sorting_entries if a[RECORDING_URI_KEY] == recording])
-        if len(potential_gts) != 1:
-            raise Exception
-        # get an arbitrary element of the set--but since we've established it has one item, we're pretty sure which one it'll be
-        recording_uris_mapped_to_true_firings[recording] = potential_gts.pop()
-        if VERBOSE: print(f"Found corresponding ground-truth {recording_uris_mapped_to_true_firings[recording]}")
-    return recording_uris_mapped_to_true_firings
-
-def filter_sortresults_per_recording(sorting_entries: List[Dict[str, Any]], recordingFile: str) -> List[Dict[str, Any]]:
-    filtered = [s for s in sorting_entries if s[RECORDING_URI_KEY] == recordingFile]
-    return filtered
-
-def get_recording_and_ground_truth(recording_uri: str, ground_truth_uri: str) -> Tuple[RecordingExtractor, SortingExtractor]:
-    if VERBOSE: print(f"Fetching extractors for recording {recording_uri} and gt-sorting {ground_truth_uri}")
-    if VERY_VERBOSE: print(f"Executing: le.LabboxEphysRecordingExtractor({recording_uri})")
-    recording_extractor: RecordingExtractor = le.LabboxEphysRecordingExtractor(recording_uri)
-    sample_rate = recording_extractor.get_sampling_frequency()
-    if VERY_VERBOSE: print(f"Found sample rate {sample_rate}")
-    firings_uri = kp.load_object(ground_truth_uri)['firings']
+@hi.function(
+    'compute_ground_truth_comparison_hi', '0.1.0',
+    kachery_support=True
+)
+def compute_ground_truth_comparison_hi(recording_uri, gt_uri, firings_uri):
+    print_per_verbose(1, f"Computing ground truth comparison for ground truth {gt_uri} and sorting {firings_uri} (recording {recording_uri})")
+    print_per_verbose(3, f'Fetching sample rate from {recording_uri}')
+    recording = le.LabboxEphysRecordingExtractor(recording_uri)
+    sample_rate = recording.get_sampling_frequency()
+    print_per_verbose(3, f'Got sample rate {sample_rate}')
+    print_per_verbose(2, f"Building sorting object for ground truth {gt_uri}")
+    gt_firings = kp.load_object(gt_uri)['firings']
+    print_per_verbose(2, f"Got ground truth firings {gt_firings}")
+    gt_sorting_obj = {
+        'sorting_format': 'mda',
+        'data': {
+            'firings': gt_firings,
+            'samplerate': sample_rate
+        }
+    }
+    gt_sorting = le.LabboxEphysSortingExtractor(gt_sorting_obj)
+    print_per_verbose(2, f"Building sorting object for sorting with firings {firings_uri}")
     sorting_obj = {
         'sorting_format': 'mda',
         'data': {
@@ -102,58 +136,51 @@ def get_recording_and_ground_truth(recording_uri: str, ground_truth_uri: str) ->
             'samplerate': sample_rate
         }
     }
-    if VERY_VERBOSE: print(f"Executing: le.LabboxEphysSortingExtractor({json.dumps(sorting_obj)})")
-    sorting_extractor: SortingExtractor = le.LabboxEphysSortingExtractor(sorting_obj)
-    return (recording_extractor, sorting_extractor)
+    sorting = le.LabboxEphysSortingExtractor(sorting_obj)
+    print_per_verbose(2, f"Executing ground-truth comparison")
+    return compare_with_ground_truth(sorting, gt_sorting)
 
-def evaluate_sorting_recording_pair(firings_file: str, recording_extr: RecordingExtractor, ground_truth: SortingExtractor) -> Tuple[Any, Any]:
-    if VERY_VERBOSE: print(f"Loading sorting extractor for {firings_file} as the firings key")
-    sorting_obj = {
-        'sorting_format': 'mda',
-        'data': {
-            'firings': firings_file,
-            'samplerate': recording_extr.get_sampling_frequency()
+def process_sorting_record(sorting_record):
+    try:
+        params = {
+            'recording_uri': sorting_record[RECORDING_URI_KEY],
+            'gt_uri': sorting_record[GROUND_TRUTH_URI_KEY],
+            'firings_uri': sorting_record[SORTING_FIRINGS_URI_KEY]
         }
+        quality_metric = hi.Job(compute_quality_metrics_hi, params).wait().return_value
+        gt_comp = hi.Job(compute_ground_truth_comparison_hi, params).wait().return_value
+        return (quality_metric, gt_comp)
+    except KeyError:
+        print(f"One of sorting/recording/gt-sorting keys missing from {json.dumps(sorting_record)}. Skipping...")
+    return (None, None)
+
+def make_comparison_entry(sorting_record, quality_metric, gt_comparison):
+    return {
+        'studyName': sorting_record['studyName'],
+        'recordingName': sorting_record['recordingName'],
+        'sorterName': sorting_record['sorterName'],
+        'quality_metric': quality_metric,
+        'ground_truth_comparison': gt_comparison
     }
-    if VERY_VERBOSE: print(f"Executing (for comparison evaluation): le.LabboxEphysSortingExtractor({json.dumps(sorting_obj)})")
-    sorter_extractor = le.LabboxEphysSortingExtractor(sorting_obj)
-    if VERY_VERBOSE: print(f"Executing quality metrics")
-    quality_metrics = compute_quality_metrics(recording_extr, sorter_extractor)
-    if VERY_VERBOSE: print(f"Executing ground-truth comparison")
-    gt_comparison = compare_with_ground_truth(sorter_extractor, ground_truth)
-    return (quality_metrics, gt_comparison)
 
 def main():
+    init_args()
+    if args['test'] != 0: print(f"\tRunning in TEST MODE--Execution will stop after processing {args['test']} sortings!\n")
+    count = 0
     sortings = load_sortings()
-    kampff_sortings = [s for s in sortings if s['studyName'] == 'paired_kampff']
     comparisons = []
-    # returns dict whose keys are the unique recording URIs and whose values are the corresponding ground-truth sorting's URI.
-    unique_recordings = get_unique_recordings_from_sortresult_list(kampff_sortings)
-    for recording_uri in unique_recordings.keys():
-        recording_extractor, ground_truth_sorting_extractor = get_recording_and_ground_truth(recording_uri, unique_recordings[recording_uri])
-        sortings_for_this_recording = filter_sortresults_per_recording(kampff_sortings, recording_uri)
-        for sorting_dict in sortings_for_this_recording:
-            try:
-                sorting_file = sorting_dict['firings']
-            except KeyError:
-                print(f"No 'firings' key for sorting_dict {json.dumps(sorting_dict)}. Skipping...")
-                continue
-            quality_metric, gt_comp = evaluate_sorting_recording_pair(sorting_file, recording_extractor, ground_truth_sorting_extractor)
-            comparison = {
-                'studyName': sorting_dict['studyName'],
-                'recordingName': sorting_dict['recordingName'],
-                'sorterName': sorting_dict['sorterName'],
-                'quality_metric': quality_metric,
-                'ground_truth_comparison': gt_comp
-            }
-            comparisons.append(comparison)
-        if ABBREVIATED_TEST: break
+
+    if args['recordingset'] is not None and args['recordingset'] != '':
+        sortings = [s for s in sortings if s['studyName'] == args['recordingset']]
+
+    for sorting_record in sortings:
+        (quality_metric, ground_truth_comparison) = process_sorting_record(sorting_record)
+        if quality_metric is None or ground_truth_comparison is None: continue
+        
+        comparisons.append(make_comparison_entry(sorting_record, quality_metric, ground_truth_comparison))
+        count += 1
+        if args['test'] != 0 and count >= args['test']: break
     print(f"Results:\n{json.dumps(comparisons, indent=4)}")
-    # This was for a different output format which we probably won't use
-    # print("\tQuality metrics:\n" + "\n".join([json.dumps(qm, indent=4) for qm in ]))
-    # print("\n\n\n")
-    # print("\tGround Truth comparisons:\n" + "\n".join(json.dumps(comp, indent=4) for comp in all_ground_truth_comparisons))
-    
 
 if __name__ == "__main__":
     main()
